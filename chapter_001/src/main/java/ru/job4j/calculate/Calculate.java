1824e1e896ed05b95caec4b7ff8961ec5abadbbe
Maksym Mateiuk <mmateiuk@cybervisiontech.com>
2019-05-09 15:32:53 +0300

    1.3. JavaDoc. Code convention[#140328]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ * 
+ * @author Maksym Mateiuk (maxymmateuk@gmail.com)
+ */
 public class Calculate {
-
+	
+	/**
+	 * Main
+	 * @param args - args
+	 */	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}	
