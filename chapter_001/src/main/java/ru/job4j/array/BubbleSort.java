b4e530e0ac0b513e0ed91398322b2697f91426d0
Maksym Mateiuk <mmateiuk@cybervisiontech.com>
2019-05-21 19:56:09 +0300

    6.5. Создать программу для сортировки массива методом перестановки.[#140308]

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -11,8 +11,8 @@ public class BubbleSort {
 
     public int[] sort(int[] array) {
         int tmp = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 1; j < array.length - 1; j++) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 1; j < array.length; j++) {
                 if (array[j - 1] > array[j]) {
                     tmp = array[j - 1];
                     array[j - 1] = array[j];
--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -21,6 +21,5 @@ public class BubbleSortTest {
         int[] expected = {0, 1, 1, 2, 3, 4, 5, 5, 7, 8};
         int[] result = bubbleSort.sort(input);
         assertThat(result, is(expected));
-        //напишите здесь тест, проверяющий сортировку массива из 10 элементов методом пузырька, например {1, 5, 4, 2, 3, 1, 7, 8, 0, 5}.
     }
 }
