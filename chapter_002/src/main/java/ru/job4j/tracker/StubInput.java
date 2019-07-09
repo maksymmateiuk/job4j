9a3422f18694a9bd4e425c6b4bf9d28387d4e7ae
Maksym Mateiuk <mmateiuk@cybervisiontech.com>
2019-07-09 13:45:27 +0300

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя[#140368]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -9,16 +9,15 @@ package ru.job4j.tracker;
  */
 public class StubInput implements Input {
 
-    private String[] answers;
+    private final String[] value;
     private int position = 0;
 
-    public StubInput(String[] answers) {
-        this.answers = answers;
+    public StubInput(final String[] value) {
+        this.value = value;
     }
 
     @Override
     public String ask(String question) {
-        System.out.println(question);
-        return answers[position++];
+        return value[position++];
     }
 }
--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,6 +2,9 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 
 /**
@@ -14,8 +17,35 @@ import static org.junit.Assert.*;
 public class StartUITest {
 
     @Test
-    public void init() {
-//        Input input = new StubInput(new String[] {"create stub task"});
-//        new StartUI(input, new Tracker()).init();
+    public void whenAddItemThenTrackerHasItem() {
+        Item item = new Item("name", "desc", System.currentTimeMillis());
+        Input input = new StubInput(new String[] {"0", item.getName(), item.getDescription(), "6"});
+        Tracker tracker = new Tracker();
+        new StartUI(input, tracker).init();
+        Item itemResult = tracker.findByName(item.getName())[0];
+        assertThat(item.getName(), is(itemResult.getName()));
+    }
+
+    @Test
+    public void whenEditItemThenItemWasEdited() {
+        Item item = new Item("name", "desc", System.currentTimeMillis());
+        Tracker tracker = new Tracker();
+        item = tracker.add(item);
+        Item toEditItem = new Item("edited", "edited desc", System.currentTimeMillis());
+        Input input = new StubInput(new String[] {"2", item.getId(), toEditItem.getName(), toEditItem.getDescription(), "6"});
+        new StartUI(input, tracker).init();
+        Item itemResult = tracker.findByName(toEditItem.getName())[0];
+        assertThat(item.getId(), is(itemResult.getId()));
+    }
+
+    @Test
+    public void whenDeleteItemThenItemDeleted() {
+        Item item = new Item("name", "desc", System.currentTimeMillis());
+        Tracker tracker = new Tracker();
+        item = tracker.add(item);
+        Input input = new StubInput(new String[] {"3", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        Item[] itemResult = tracker.findByName(item.getName());
+        assertThat(itemResult, is(new Item[]{}));
     }
 }
