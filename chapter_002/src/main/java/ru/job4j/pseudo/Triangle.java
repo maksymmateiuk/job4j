9895bea5a565874f2abd2a87e91058afbae4d33f
Maksym Mateiuk <mmateiuk@cybervisiontech.com>
2019-07-12 08:50:33 +0300

    task_140369 - add lineSeparator for shapes

--- chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
+++ chapter_002/src/main/java/ru/job4j/pseudo/Triangle.java
@@ -12,9 +12,9 @@ public class Triangle implements Shape {
     @Override
     public String draw() {
         StringBuilder pic = new StringBuilder();
-        pic.append("  +");
-        pic.append(" +++");
-        pic.append("+++++");
+        pic.append("  +").append(System.lineSeparator());
+        pic.append(" +++").append(System.lineSeparator());
+        pic.append("+++++").append(System.lineSeparator());
         return pic.toString();
     }
 }
--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -39,11 +39,11 @@ public class PaintTest {
         assertThat(
                 new String(out.toByteArray()),
                 is(new StringBuilder()
-                                .append("++++")
-                                .append("+     +")
-                                .append("+     +")
-                                .append("++++")
-                                .append(System.lineSeparator())
+                        .append("++++").append(System.lineSeparator())
+                        .append("+  +").append(System.lineSeparator())
+                        .append("+  +").append(System.lineSeparator())
+                        .append("++++").append(System.lineSeparator())
+                        .append(System.lineSeparator())
                                 .toString()
                 )
         );
@@ -55,9 +55,9 @@ public class PaintTest {
         assertThat(
                 new String(out.toByteArray()),
                 is(new StringBuilder()
-                        .append("  +")
-                        .append(" +++")
-                        .append("+++++")
+                        .append("  +").append(System.lineSeparator())
+                        .append(" +++").append(System.lineSeparator())
+                        .append("+++++").append(System.lineSeparator())
                         .append(System.lineSeparator())
                         .toString()
                 )
